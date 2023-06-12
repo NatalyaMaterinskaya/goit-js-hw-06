811b70de63ec6cec57b0063063b70069c35c733b
NatalyaMaterinskaya <natulka-91@ukr.net>
2023-06-12 20:39:24 +0300

    Finished task-03

---
+++ js/task-02.js
@@ -0,0 +1,25 @@
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+
+const list = document.querySelector("#ingredients");
+
+console.dir(list);
+
+const createItem = ingredient => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.classList.add("item");
+  return item;
+};
+
+const markup = ingredients.map((el) => createItem(el));
+
+list.append(...markup);
+
+console.dir(list);
